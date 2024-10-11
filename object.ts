a29f7f065fa9a9f763cf0f9a54f6d3463e5da5ab
demenchuk sophie <demenchuk sophie>
2024-10-11 11:06:42 +0300

    add: machineSpecifications

---
+++ object.ts
@@ -0,0 +1,17 @@
+const machineSpecifications = {
+    title: "Mazda6",
+    salon: "dark",
+    securitySystems: "ABS",
+    cushions: "lateral",
+    aidSystemsl: "rain sensor",
+    exterior: "towbar",
+    heating: "seats",
+    multimedia: "CD or MP3",
+    comfort: "cruise control",
+    headlights: "LED"
+}
+
+machineSpecifications.salon = "light"
+
+
+console.log(machineSpecifications.comfort)
---
+++ veriable.ts
@@ -0,0 +1,6 @@
+const f_number = 1987
+const s_number = 5
+const result = f_number+s_number
+
+
+console.log(result)
