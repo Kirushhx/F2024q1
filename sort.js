e54f9e4f38417f105bdbbefe56986221c5c6b58b
demenchuk sophie <demenchuk sophie>
2024-11-22 11:20:15 +0300

    add: sort.js

---
+++ sort.js
@@ -0,0 +1,7 @@
+const words =['apple', 'dog', 'cat', 'beyonce', 'pdidi', 'sun', 'spring', 'love', 'ksusha', 'JK']
+const resultUp = words.sort((a,b)=> a-b)
+//console.log(resultUp)
+
+const resultDown = words.sort((a,b)=> a-b)
+const reversed = resultDown.reverse()
+console.log(resultDown)
